4886ea97c3f006f7bf867c0ad009433bdbcab597
ValeriyaMix <lerka-mishka@yandex.ru>
2023-01-13 22:33:17 +0000

    WIP

--- SupplyStacks/Program.cs
+++ SupplyStacks/Program.cs
@@ -16,8 +16,8 @@ namespace SupplyStacks
             
 
             var ListOfCrates = new Dictionary<int, string>();
-            var listOfCrates = new Dictionary<int, List<string>>();
-            int numOfPos = 0;
+            var dictOfCrates = new Dictionary<int, List<string>>();
+            int numOfPos = 1;
 
             List<string> moveList = new List<string>();
             List<string> fromList = new List<string>();
@@ -26,37 +26,37 @@ namespace SupplyStacks
 
             var rx = new Regex(@"\[[a-zA-Z]]", RegexOptions.Compiled);
             var rxx = new Regex(@"\s[^1-9]", RegexOptions.Compiled);
+            var rxxx = new Regex(@"\[1-9]", RegexOptions.Compiled);
 
             int totalNumOfColumns = (lines[2].Count() - 3) / 4 + 1;
-
-            
-
-
-
 
             foreach (string line in lines)
             {
                              
                 for (int i = 0; i < line.Length; i = i + 4)
                 {
-                    
-                    if (rx.IsMatch(line.Substring(i, 3)) || rxx.IsMatch(line.Substring(i, 3)))
-                    {
+                    //Console.WriteLine("line of substring is {0}", line.Substring(i, 3));
 
-                        if (listOfCrates.ContainsKey(numOfPos))
+                    if (rx.IsMatch(line.Substring(i, 3))) 
+                    {
+                        //Console.WriteLine("i add a letter {0}", line.Substring(i, 3));
+
+                        if (dictOfCrates.ContainsKey(numOfPos))
                         {
-                            listOfCrates[numOfPos].Add(line.Substring(i + 1, 1));
+                            dictOfCrates[numOfPos].Add(line.Substring(i + 1, 1));
+                            //Console.WriteLine(numOfPos);
                         }
                         else
                         {
-                            listOfCrates.Add(numOfPos, new List<string>());
-                            listOfCrates[numOfPos].Add(line.Substring(i + 1, 1));
+                            dictOfCrates.Add(numOfPos, new List<string>());
+                            dictOfCrates[numOfPos].Add(line.Substring(i + 1, 1));
+                            //Console.WriteLine(numOfPos);
                         }
                         
                         if (counter == totalNumOfColumns - 1)
                         {
                             counter = 0;
-                            numOfPos = 0;
+                            numOfPos = 1;
                         }
                         else
                         {
@@ -65,14 +65,31 @@ namespace SupplyStacks
                         }
                         
                     }
+                    else if (rxx.IsMatch(line.Substring(i, 3)))
+                    {
+                        //Console.WriteLine("i add a letter nothing", line.Substring(i, 3));
+
+                        if (counter == totalNumOfColumns - 1)
+                        {
+                            counter = 0;
+                            numOfPos = 1;
+                        }
+                        else
+                        {
+                            counter++;
+                            numOfPos++;
+                        }
+                        
+                    }
+                    
                     else
                     {
+                        //Console.WriteLine("populate arrays");
                         if (line.Contains("move"))
                         {
                             string[] lineSplit = line.Split(" ");
-                            Console.WriteLine("move: {0} from: {1} to: {2}", lineSplit[1], lineSplit[3], lineSplit[5]);
+                            //Console.WriteLine("move: {0} from: {1} to: {2}", lineSplit[1], lineSplit[3], lineSplit[5]);
 
-                            Console.WriteLine("\n");
                             moveList.Add(lineSplit[1]);
                             fromList.Add(lineSplit[3]);
                             toList.Add(lineSplit[5]);
@@ -84,8 +101,51 @@ namespace SupplyStacks
                 }
 
             }
-            //ListOfCrates.Remove(ListOfCrates.Keys.Last());
-            Console.WriteLine("Length of dictionary:{0}", listOfCrates.Count);
+            //for (int i = 1; i <= totalNumOfColumns; i++)
+            //{
+            //    Console.WriteLine("column {0}", i);
+            //    foreach (var item in dictOfCrates[i])
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+            //    Console.WriteLine("\n");
+            //}
+
+
+            for (int i = 0; i < moveList.Count(); i++)
+            {
+                int numOfMovingCaters = Convert.ToInt32(moveList[i]);
+                //Console.WriteLine("the num of moving caters: {0}", numOfMovingCaters);
+                //Console.WriteLine("\n");
+                var theCaterList = dictOfCrates[Convert.ToInt32(fromList[i])];
+
+
+                for (int j = 0; j < numOfMovingCaters; j++)
+                {
+                    dictOfCrates[Convert.ToInt32(toList[i])].Add(theCaterList[0 + j]);
+                    
+                    //Console.WriteLine("i m adding: {0}", theCaterList[0 + j]);
+                }
+                dictOfCrates[Convert.ToInt32(fromList[i])].RemoveRange(0, numOfMovingCaters);
+
+                //Console.WriteLine("letters that are left after moving:");
+                //foreach (var theCater in dictOfCrates[Convert.ToInt32(fromList[i])])
+                //{
+                //    Console.WriteLine(theCater);
+                //}
+
+            }
+
+            for (int i = 1; i <= totalNumOfColumns; i++)
+            {
+                Console.WriteLine($"column {i}");
+                //Console.WriteLine(dictOfCrates[i].First());
+                foreach (var item in dictOfCrates[i])
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
 
         }
     }
