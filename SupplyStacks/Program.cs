ab853962e4b64dd0e98f757449598fe0425ce0cf
ValeriyaMix <lerka-mishka@yandex.ru>
2023-01-14 23:48:42 +0000

    Refactor code. Add method for Part 2 task.

--- SupplyStacks/Program.cs
+++ SupplyStacks/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics.Metrics;
+using System.Reflection;
 using System.Runtime.ConstrainedExecution;
 using System.Text.RegularExpressions;
 
@@ -9,30 +10,49 @@ namespace SupplyStacks
     {
         static void Main(string[] args)
         {
-            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\valer\source\repos\AdventOfCode2022\SupplyStacks\input.txt");
+            string[] lines = readFromTheFile("SupplyStacks");
 
-            int counter = 0;
-
-            var dictOfCrates = new Dictionary<int, List<string>>();
-            int numOfPos = 1;
-
-            List<string> moveList = new List<string>();
-            List<string> fromList = new List<string>();
-            List<string> toList = new List<string>();
+            List<string> howMuchToMoveList = new List<string>();
+            List<string> fromColumnList = new List<string>();
+            List<string> toColumnList = new List<string>();
 
 
-            var rx = new Regex(@"\[[a-zA-Z]]", RegexOptions.Compiled);
-            var rxx = new Regex(@"\s[^1-9]", RegexOptions.Compiled);
-            var rxxx = new Regex(@"\[1-9]", RegexOptions.Compiled);
+            var lettersWithBrackets = new Regex(@"\[[a-zA-Z]]", RegexOptions.Compiled);
+            var spaceWithNumbers = new Regex(@"\s[^1-9]", RegexOptions.Compiled);
 
             int totalNumOfColumns = (lines[2].Count() - 3) / 4 + 1;
 
+            var dictOfCrates = createDictFromData(lines, lettersWithBrackets, spaceWithNumbers, totalNumOfColumns,
+                                                    howMuchToMoveList, fromColumnList, toColumnList);
+
+            reverseDict(dictOfCrates, totalNumOfColumns);
+
+            movingCratesPart2(dictOfCrates, howMuchToMoveList, fromColumnList, toColumnList);
+
+            printResult(dictOfCrates, totalNumOfColumns);
+        }
+
+        public static string[] readFromTheFile(string folderName)
+        {
+            return System.IO.File.ReadAllLines($@"C:\Users\valer\source\repos\AdventOfCode2022\{folderName}\input.txt");
+            
+        }
+
+        public static Dictionary<int, List<string>> createDictFromData(string[] lines, Regex lettersWithBrackets,
+                                                                        Regex spaceWithNumbers, int totalNumOfColumns,
+                                                                        List<string> howMuchToMoveList,
+                                                                        List<string> fromColumnList, List<string> toColumnList)
+        {
+            int counter = 0;
+            int numOfPos = 1;
+            var dictOfCrates = new Dictionary<int, List<string>>();
+
             foreach (string line in lines)
             {
-                             
+
                 for (int i = 0; i < line.Length; i = i + 4)
                 {
-                    if (rx.IsMatch(line.Substring(i, 3))) 
+                    if (lettersWithBrackets.IsMatch(line.Substring(i, 3)))
                     {
                         if (dictOfCrates.ContainsKey(numOfPos))
                         {
@@ -43,7 +63,7 @@ namespace SupplyStacks
                             dictOfCrates.Add(numOfPos, new List<string>());
                             dictOfCrates[numOfPos].Add(line.Substring(i + 1, 1));
                         }
-                        
+
                         if (counter == totalNumOfColumns - 1)
                         {
                             counter = 0;
@@ -54,9 +74,9 @@ namespace SupplyStacks
                             counter++;
                             numOfPos++;
                         }
-                        
+
                     }
-                    else if (rxx.IsMatch(line.Substring(i, 3)))
+                    else if (spaceWithNumbers.IsMatch(line.Substring(i, 3)))
                     {
                         if (counter == totalNumOfColumns - 1)
                         {
@@ -68,58 +88,87 @@ namespace SupplyStacks
                             counter++;
                             numOfPos++;
                         }
-                        
+
                     }
-                    
+
                     else
                     {
                         if (line.Contains("move"))
                         {
                             string[] lineSplit = line.Split(" ");
 
-                            moveList.Add(lineSplit[1]);
-                            fromList.Add(lineSplit[3]);
-                            toList.Add(lineSplit[5]);
+                            howMuchToMoveList.Add(lineSplit[1]);
+                            fromColumnList.Add(lineSplit[3]);
+                            toColumnList.Add(lineSplit[5]);
 
                             break;
                         }
-                        
+
                     }
                 }
 
             }
 
+            return dictOfCrates;
+        }
+
+        public static Dictionary<int, List<string>> reverseDict(Dictionary<int, List<string>> dictOfCrates, int totalNumOfColumns)
+        {
             //Reversing lists in dictionary
 
             for (int i = 1; i <= totalNumOfColumns; i++)
             {
                 dictOfCrates[i].Reverse();
             }
+            return dictOfCrates;
+        }
 
 
-            for (int i = 0; i < moveList.Count(); i++)
+        public static void movingCrates(Dictionary<int, List<string>> dictOfCrates, List<string>  howMuchToMoveList,
+                                        List<string> fromColumnList, List<string> toColumnList)
+        {
+            for (int i = 0; i < howMuchToMoveList.Count(); i++)
             {
-                int numOfMovingCaters = Convert.ToInt32(moveList[i]);
-                
-                var theCaterList = dictOfCrates[Convert.ToInt32(fromList[i])];
+                int numOfMovingCaters = Convert.ToInt32(howMuchToMoveList[i]);
+
+                var theCaterList = dictOfCrates[Convert.ToInt32(fromColumnList[i])];
 
                 for (int j = 0; j < numOfMovingCaters; j++)
                 {
-                    dictOfCrates[Convert.ToInt32(toList[i])].Add(theCaterList[theCaterList.Count() - 1 - j]);
+                    dictOfCrates[Convert.ToInt32(toColumnList[i])].Add(theCaterList[theCaterList.Count() - 1 - j]);
 
                 }
                 int startRemovingFrom = theCaterList.Count() - numOfMovingCaters;
-                dictOfCrates[Convert.ToInt32(fromList[i])].RemoveRange(startRemovingFrom, numOfMovingCaters);
-
+                dictOfCrates[Convert.ToInt32(fromColumnList[i])].RemoveRange(startRemovingFrom, numOfMovingCaters);
             }
+        }
 
+        public static void movingCratesPart2(Dictionary<int, List<string>> dictOfCrates, List<string> howMuchToMoveList,
+                                        List<string> fromColumnList, List<string> toColumnList)
+        {
+            for (int i = 0; i < howMuchToMoveList.Count(); i++)
+            {
+                int numOfMovingCaters = Convert.ToInt32(howMuchToMoveList[i]);
+
+                var theCaterList = dictOfCrates[Convert.ToInt32(fromColumnList[i])];
+
+                for (int j = 0; j < numOfMovingCaters; j++)
+                {
+                    dictOfCrates[Convert.ToInt32(toColumnList[i])].Add(theCaterList[theCaterList.Count() - (numOfMovingCaters - j)]);
+
+                }
+                int startRemovingFrom = theCaterList.Count() - numOfMovingCaters;
+                dictOfCrates[Convert.ToInt32(fromColumnList[i])].RemoveRange(startRemovingFrom, numOfMovingCaters);
+            }
+        }
+
+        public static void printResult(Dictionary<int, List<string>> dictOfCrates, int totalNumOfColumns)
+        {
             for (int i = 1; i <= totalNumOfColumns; i++)
             {
                 Console.WriteLine($"column {i}");
                 Console.WriteLine(dictOfCrates[i].Last());
             }
-
-
         }
     }
 }
