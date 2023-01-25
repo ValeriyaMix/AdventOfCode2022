10143ebe3646f84e4dce5357f413a78abf15bd9d
ValeriyaMix <lerka-mishka@yandex.ru>
2023-01-25 22:58:07 +0000

    WIP

--- NoSpaceLeftOnDevice/Program.cs
+++ NoSpaceLeftOnDevice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.Metrics;
 using System.Linq.Expressions;
@@ -15,85 +16,140 @@ namespace NoSpaceLeftOnDevice
             string[] line = readFromTheFile("NoSpaceLeftOnDevice");
             
             var structure = new Dictionary<string, List<int>>();
-            char[] charsToTrim = {'c', 'd', ' ', 'i', 'r', '$'};
-            List<string> sequenceList = new List<string>();
 
             string currentFolderName = "";
-            int positionOfCurrentFolder = 0;
 
             string folderToAdd = "";
-            int sum = 0;
-            int counting = 0;
+            string parentFolderName = "";
 
 
             var dictOfPositions = new Dictionary<int, string>();
             var dictOfSums = new Dictionary<string, List<string>>();
             var dictOfFolders = new Dictionary<string, List<string>>();
 
+            List<string> listOfParents = new List<string>();
+            int positionOfParentFolder = 0;
 
+            currentFolderName = line[0].Split(" ")[2];
+            parentFolderName = line[0].Split(" ")[2];
 
-            for (int i = 0; i < line.Length; i++)
+            for (int i = 1; i < line.Length; i++)
             {
                 if (line[i].Substring(0, 4) == "$ cd")
                 {
                     if (line[i] == "$ cd ..")
                     {
-                        positionOfCurrentFolder = positionOfCurrentFolder - 1;
-                        currentFolderName = dictOfPositions[positionOfCurrentFolder];
                         
+                        if (currentFolderName != "/")
+                        {
+                            positionOfParentFolder = listOfParents.FindIndex(x => x == currentFolderName) - 1;
+                            if (dictOfFolders[listOfParents[positionOfParentFolder]].Contains(currentFolderName))
+                            {
+                                parentFolderName = listOfParents[positionOfParentFolder];
+                                currentFolderName = parentFolderName;
+                            }
+                            else
+                            {
+                                while (!dictOfFolders[listOfParents[positionOfParentFolder]].Contains(currentFolderName))
+                                {
+                                    positionOfParentFolder = positionOfParentFolder - 1;
+                                    parentFolderName = listOfParents[positionOfParentFolder];
+                                    currentFolderName = parentFolderName;
+                                }
+                            }
+                            
+                            
+                        }
                         
+                        Console.WriteLine("Step back Parent folder {0}", parentFolderName);
+                        Console.WriteLine("Step back Current folder {0}", currentFolderName);
+                        Console.WriteLine(positionOfParentFolder);   
                     }
                     else
                     {
+                        parentFolderName = currentFolderName;
                         currentFolderName = line[i].Split(" ")[2];
-                        positionOfCurrentFolder++;
-                        dictOfPositions[positionOfCurrentFolder] = currentFolderName;
-                        isItInDict(dictOfFolders, currentFolderName);
+
+                        Console.WriteLine("Step forward Parent folder {0}", parentFolderName);
+                        Console.WriteLine("Step forward Current folder {0}", currentFolderName);
                         
+
+                        if (dictOfFolders[parentFolderName].Contains(currentFolderName))
+                        {
+                            Console.WriteLine($"Folder {currentFolderName} is in the folder {parentFolderName}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Folder {currentFolderName} is not in the folder {parentFolderName}");
+                        }
+                        Console.WriteLine("\n");
+
                     }
-                    
-                    //Console.WriteLine($"Currentfolder name: {currentFolderName}");
-                    //Console.WriteLine( "\n");
+
                 }
                 else if (line[i].Substring(0, 3) == "dir")
                 {
                     folderToAdd = line[i].Split(" ")[1];
-                    dictOfFolders[currentFolderName].Add(folderToAdd);
+                    if (currentFolderName == "/")
+                    {
+                        dictOfFolders[currentFolderName].Add(folderToAdd);
+                    }
+                    else
+                    {
+                        dictOfFolders[parentFolderName].Add(folderToAdd);
+                        dictOfFolders[currentFolderName].Add(folderToAdd);
+                    }
+                   
                     //Console.WriteLine($"Folder that was added {folderToAdd}");
                 }
                 else if (line[i].Any(char.IsDigit))
                 {
                     string dig = line[i].Split()[0];
-                    
-                    isItInDict(dictOfSums, currentFolderName);
-                    isItInDict(dictOfFolders, currentFolderName, dig);
-                    dictOfSums[currentFolderName].Add(dig);
-                    //Console.WriteLine($"Sum that was added {dig} to the folder {currentFolderName}");
+                    if (currentFolderName == "/")
+                    {
+                        dictOfFolders[currentFolderName].Add(dig);
+                    }
+                    else
+                    {
+                        dictOfFolders[parentFolderName].Add(dig);
+                        dictOfFolders[currentFolderName].Add(dig);
+                    }
                 }
                 else
                 {
+                    if (!dictOfFolders.ContainsKey(currentFolderName))
+                    {
+                        dictOfFolders.Add(currentFolderName, new List<string>());
+                    }
+
+                    if (!listOfParents.Contains(currentFolderName))
+                    {
+                        listOfParents.Add(currentFolderName);
+                    }
 
                 }
-                //Console.WriteLine("\n");
 
             }
 
 
 
+            // Iterating through list of folders
+            //foreach (var item in dictOfFolders)
+            //{
+            //    Console.WriteLine($"External folder {item.Key}:");
+            //    foreach (var val in item.Value)
+            //    {
 
-            foreach (var item in dictOfFolders)
-            {
-                Console.WriteLine($"External folder {item.Key}:");
-                foreach(var val in item.Value)
-                {
-                    Console.WriteLine($"Values of the folder {item.Key} are: {val}");
-                }
-                Console.WriteLine("\n");
-            }
+            //        Console.WriteLine($"Values of the folder {item.Key} are: {val}");
+            //    }
+            //    Console.WriteLine("\n");
+            //}
+
+
         }
         public static string[] readFromTheFile(string folderName)
         {
-            return System.IO.File.ReadAllLines($@"C:\Users\valer\source\repos\AdventOfCode2022\{folderName}\input.txt");
+            return System.IO.File.ReadAllLines($@"C:\Users\valer\source\repos\AdventOfCode2022\{folderName}\input1.txt");
 
         }
 
@@ -101,6 +157,7 @@ namespace NoSpaceLeftOnDevice
         {
             if (structure.ContainsKey(folderName))
             {
+               
                 return folderName;
                 
             }
@@ -126,24 +183,5 @@ namespace NoSpaceLeftOnDevice
         }
 
 
-        public static int findUniqueSequence(int markerLength, string line)
-        {
-            int numCount = markerLength;
-            for (int i = 0; i < line.Count(); i++)
-            {
-                string diffSequence = line.Substring(i, markerLength);
-
-                if (diffSequence.Distinct().Count() == markerLength)
-                {
-                    return numCount;
-                }
-                else
-                {
-                    numCount++;
-                }
-            }
-
-            return numCount;
-        }
     }
 }
